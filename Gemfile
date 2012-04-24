d343559e83221876c7eced481a6575a8650ede8f
Ibrahim Kusnanto <ibkusnanto@gmail.com>
2012-04-24 23:58:38 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'jquery-rails'
+gem 'haml'
+gem 'haml-rails'
+gem 'thin'
+gem 'high_voltage'
+gem 'blue_sparks'
+
+gem 'execjs'
+gem 'therubyracer'
+gem 'bootstrap-sass'
+gem 'sass-rails'
+gem 'coffee-rails'
+gem 'uglifier'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNameIsIbrahim::Application.config.secret_token = '3957d6a84ba65a2aa038e2ff386ebff6fa11faadf1f55267f53072e3535225f95e740caca387c40b6fbdef3ff2556173632c3be4efd21c5f6b5e0ec5566795f2'
